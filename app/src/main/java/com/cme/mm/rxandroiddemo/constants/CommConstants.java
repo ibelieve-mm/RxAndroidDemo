2daed483f46b1b6bbcdee925df14eec28ad52c27
ibelieve-mm <ibelieve1210@163.co>
2016-11-01 14:22:37 +0800

    merge code，更改服务器

--- app/src/main/java/com/cme/mm/rxandroiddemo/constants/CommConstants.java
+++ app/src/main/java/com/cme/mm/rxandroiddemo/constants/CommConstants.java
@@ -12,7 +12,7 @@ public class CommConstants {
     public static final String APP_BASE_FOLDER_PATH = "/RxAndroid";
     public static final String KEY_PASSWORD_AES = "wuliupai40085656"; // AES密码
 
-    public static final String DEMO_SERVER = "http://192.168.1.129:8080/";//自建服务器
+    public static final String DEMO_SERVER = "http://192.168.1.125:8080/";//自建服务器
     public static final String LOGIN_PATH = "http://interface.wuliupai.cn/";//work服务器线上
     //        public static final String LOGIN_PATH = "http://192.168.1.13:8080/completemobilewlp/";//work服务器线下
 }
